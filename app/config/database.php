21c72dfc5f3b132efe92bc4f19679c00c54ea485
mohaiminul-sust <mohaiminul.sust@csesociety.org>
2015-09-23 00:14:46 +0600

    admindev

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'ecommdb',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'admindev',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
